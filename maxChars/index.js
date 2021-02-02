5612324340754e73a34ecaf58ade0f5d79826ffc
ShuvranilSaha <subranil.saha@gmail.com>
2021-02-02 11:13:41 +0530

    added maxChars problem

---
+++ maxChars/index.js
@@ -0,0 +1,27 @@
+// basic for loop
+
+function maxChar(string) {
+
+    const charMap = {}
+    let max = 0;
+    let maxChar = ''
+
+    for (let char of string) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char]
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
+
+
+module.exports = maxChar;
