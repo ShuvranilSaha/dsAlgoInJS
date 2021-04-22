4d7aca67ddb16e9722d44739b33702cf83331c07
ShuvranilSaha <subranil.saha@gmail.com>
2021-04-22 20:49:13 +0530

    added vowels problem

---
+++ find_voewels/index.js
@@ -0,0 +1,12 @@
+// function vowels(str) {
+//     let count = 0;
+//     const checker = "aeiou";
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             count ++;
+//         }
+//     }
+//     return count;
+// }
+
+module.exports = "vowels";
