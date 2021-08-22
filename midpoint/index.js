50546d00f2d86f5ba31cf8edfdd32d85bb9ab6e1
ShuvranilSaha <subranil.saha@gmail.com>
2021-08-22 11:31:59 +0530

    find midpoint in linked list

---
+++ midpoint/index.js
@@ -0,0 +1,18 @@
+
+
+class LinkedList {
+
+        // find point of linkedList
+        midPoint(list) {
+            let slow = list.getFirst();
+            let fast = list.getFirst();
+   
+            while(fast.next && fast.next.next) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+}
+
+        module.exports = {}
